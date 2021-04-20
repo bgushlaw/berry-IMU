58430aa0f6b5582b1cf961ecf03895a9393de414
bgushlaw <79597063+bgushlaw@users.noreply.github.com>
2021-04-20 16:08:36 -0600

    Update get_sensor_data.py

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -20,7 +20,7 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 i=0
 x_vals,ACCx_vals,ACCy_vals,ACCz_vals=[],[],[],[]
-fig.show()
+
 
 while True:
   
@@ -45,7 +45,7 @@ while True:
     ax.plot(x_vals,ACCy_vals,color='r')
     ax.plot(x_vals,ACCz_vals,color='g')
     ax.set_xlim(left=max(0, i-50), right=i+50)
-    fig.canvas.draw()
+    fig.show()
     time.sleep(.02)
     i += 1
    
