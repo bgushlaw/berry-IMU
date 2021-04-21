05c571f42ecc186c0c59aabc6aac577fb5b275e4
bgushlaw <79597063+bgushlaw@users.noreply.github.com>
2021-04-20 23:50:25 -0600

    Update get_sensor_data.py

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -4,8 +4,8 @@ import math
 import IMU
 import datetime
 import os
-import matplotlib
-matplotlib.use('Agg')
+#import matplotlib
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
