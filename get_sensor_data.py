add3bc7f21e101bca114099881b40da6b8b7d00d
bgushlaw <79597063+bgushlaw@users.noreply.github.com>
2021-04-21 21:51:40 -0600

    Update get_sensor_data.py

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -7,8 +7,13 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+import pandas as pd
+import os.path
+from os import path
+import piDataWrangler
 
-plt.style.use('fivethirtyeight')
+
+
 
 IMU.detectIMU()     #Detect if BerryIMU is connected.
 if(IMU.BerryIMUversion == 99):
@@ -18,56 +23,43 @@ IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
 
 
     
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
-i=0
-
-time.sleep(1) # wait for mpu9250 sensor to settle
+window_size=128
 
 
 # prepping for visualization
 berry_imu_str = ['accel-x','accel-y','accel-z','gyro-x','gyro-y','gyro-z']
+Old_Feature_list = ['acc-X', 'acc-Y', 'acc-Z','gyro-X', 'gyro-Y', 'gyro-Z']
+New_Feature_list = ['Bacc-X', 'Bacc-Y', 'Bacc-Z', 'Gacc-X', 'Gacc-Y', 'Gacc-Z', 'gyro-X', 'gyro-Y', 'gyro-Z']
+Final_Feature_list = ['tBodyAcc-mean-X','tBodyAcc-mean-Y','tBodyAcc-mean-Z','tBodyAcc-std-X', 'tBodyAcc-std-Y', 'tBodyAcc-std-Z','tBodyAcc-max-X','tBodyAcc-max-Y','tBodyAcc-max-Z','tBodyAcc-min-X','tBodyAcc-min-Y','tBodyAcc-min-Z','tGravityAcc-mean-X','tGravityAcc-mean-Y','tGravityAcc-mean-Z','tGravityAcc-std-X','tGravityAcc-std-Y','tGravityAcc-std-Z','tGravityAcc-max-X','tGravityAcc-max-Y','tGravityAcc-max-Z','tGravityAcc-min-X','tGravityAcc-min-Y','tGravityAcc-min-Z','tBodyGyro-mean-X','tBodyGyro-mean-Y','tBodyGyro-mean-Z','tBodyGyro-std-X','tBodyGyro-std-Y','tBodyGyro-std-Z','tBodyGyro-max-X','tBodyGyro-max-Y','tBodyGyro-max-Z','tBodyGyro-min-X','tBodyGyro-min-Y','tBodyGyro-min-Z']
+#x_vals,ACCx_vals,ACCy_vals,ACCz_vals=[],[],[],[]
 
-x_vals,ACCx_vals,ACCy_vals,ACCz_vals=[],[],[],[]
-
-def animate(i,x_vals,ACCx_vals,ACCy_vals,ACCz_vals):
-
-    plt.cla()
-
-    plt.plot(x_vals, ACCx_vals, label='Channel 1')
-    plt.plot(x_vals, ACCy_vals, label='Channel 2')
-    plt.plot(x_vals, ACCz_vals, label='Channel 3')
-    
-   
-    
-    plt.legend(loc='upper left')
-    plt.tight_layout()
-
+new_Path='PiData/'
+file_name=new_Path+'TestingIt'
+save_data=pd.DataFrame(columns=Final_Feature_list)
+save_data.to_csv(file_name+'.csv',index=True)
+df=save_data
 
 while True:
   
     
-       
-    ACCx = IMU.readACCx()*0.061/1000
-    ACCy = IMU.readACCy()*0.061/1000
-    ACCz = IMU.readACCz()*0.061/1000
-    GYRx = IMU.readGYRx()*0.07
-    GYRy = IMU.readGYRy()*0.07
-    GYRz = IMU.readGYRz()*0.07
-  
+    for x in range(0,window_size)   
+        ACCx = IMU.readACCx()*2*0.061/1000
+        ACCy = IMU.readACCy()*2*0.061/1000
+        ACCz = IMU.readACCz()*2*0.061/1000
+        GYRx = IMU.readGYRx()*0.07
+        GYRy = IMU.readGYRy()*0.07
+        GYRz = IMU.readGYRz()*0.07
+        
+        df=df.append(df)
+        time.sleep(.02)
+        
+   wf=piDataWrangler.Create_Features(df,file_name,Old_Feature_list,New_Feature_list,Final_Feature_list)
+   print(wf)
     
-    print('ACCx:',ACCx,'ACCy:',ACCy,'ACCz:',ACCz,'GYRx:',GYRx,'GYRy:',GYRy,'GYRz:',GYRz)
-    
-    x_vals.append(i)
-    ACCx_vals.append(ACCx)
-    ACCy_vals.append(ACCy)
-    ACCz_vals.append(ACCz)
-    
+    #print('ACCx:',ACCx,'ACCy:',ACCy,'ACCz:',ACCz,'GYRx:',GYRx,'GYRy:',GYRy,'GYRz:',GYRz)
     
 
-    ani = FuncAnimation(plt.gcf(), animate(i,x_vals,ACCx_vals,ACCy_vals,ACCz_vals), interval=1000)
-    i += 1
-    plt.tight_layout()
-    plt.show()
-    time.sleep(.02)
+    
+
+
    
