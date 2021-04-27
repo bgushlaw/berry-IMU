c3fb926eced2eb5444ed2a817b7de50409f32e7b
bgushlaw <79597063+bgushlaw@users.noreply.github.com>
2021-04-27 09:57:09 -0600

    Update get_sensor_data.py

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -38,7 +38,7 @@ file_name=new_Path+'TestingIt'
 save_data=pd.DataFrame(columns=Final_Feature_list)
 save_data.to_csv(file_name+'.csv',index=True)
 #df = pd.DataFrame(0, index=np.arange(128), columns=Old_Feature_list)
-a = numpy.zeros(shape=(window_size,6))
+a = np.zeros(shape=(window_size,6))
 
 while True:
     tic=time.time()
